e211791d3706f090e4b0e4664351689daf0810ec
bacaron <bacaron@imail.iu.edu>
2017-11-02 21:03:02 -0400

    Update dwiAlignT1NODDI.m

--- dwiAlignT1NODDI.m
+++ dwiAlignT1NODDI.m
@@ -1,4 +1,4 @@
-function out = dwiAlignT1NODDI()
+function out = dwiAlign2T1()
 
 %%  Will align multi-shell dwi to ACPC aligned T1 and rotate the bvecs.  Needs vistasoft ((C) Vista lab, Stanford University)
 
@@ -11,15 +11,35 @@ case 'VM'
   addpath(genpath('/usr/local/vistasoft'))
 end
 
+config = loadjson('config.json');
+
+if isempty(config.resolution)
+  % to find resolution
+  disp('loading dwi resolution')
+  dwi = niftiRead(config.dwi);
+  outresolution = dwi.pixdim(1:3);
+  clear dwi
+else
+  outresolution = str2num(config.resolution);
+end
+
+switch config.interpolation
+  case {'linear'}
+    interpolation = [0 0 0 0 0 0];
+  case {'trilinear'}
+    interpolation = [1 1 1 0 0 0];
+  case {'spline'}
+    interpolation = [7 7 7 0 0 0];
+  otherwise
+    error('Interpolation method not defined: %s.',config.interpolation);
+end 
 
 % Set directories
-config = loadjson('config.json');
 dwiRaw = fullfile(config.dwi);
 bvecs_pre = fullfile(config.bvecs);
 b0 = fullfile('nodif_brain.nii.gz');
 t1 = fullfile(config.t1);
 outAcpcTransform = fullfile('acpcTransform.mat');
-outMm = [2 2 2];
 outDwi = fullfile('data_acpc.nii.gz');
 outBvecs = fullfile('bvecs_rot');
 
@@ -27,7 +47,7 @@ outBvecs = fullfile('bvecs_rot');
 dtiRawAlignToT1(b0,t1,outAcpcTransform);
 
 % Run Alignment
-dtiRawResample(dwiRaw,[],outAcpcTransform,outDwi,[0 0 0 0 0 0],[2 2 2]);
+dtiRawResample(dwiRaw,[],outAcpcTransform,outDwi,interpolation,outresolution);
 
 % Rotate bvecs
 bvecs = dtiRawReorientBvecs(bvecs_pre,[],outAcpcTransform,outBvecs);
