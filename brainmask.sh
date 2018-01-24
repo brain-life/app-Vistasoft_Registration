eb5dac5a66c5eff5913333fd93c648d04bba8fb4
Brad Caron <bacaron@imail.iu.edu>
2018-01-24 16:13:16 -0500

    dockerization changes

--- brainmask.sh
+++ brainmask.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Setting file paths"
 # Grab the config.json inputs
 dwi=`jq -r '.dwi' config.json`;
@@ -6,12 +7,30 @@ bvals=`jq -r '.bvals' config.json`;
 #t1=`./jq -r '.t1' config.json`;
 echo "Files loaded"
 
-# Create b0 image
-select_dwi_vols \
-	${dwi} \
-	${bvals} \
-	nodif.nii.gz \
-	0;
+source ${FSLDIR}/etc/fslconf/fsl.sh
+
+d=$dwi
+b=`cat $bvals`
+o=nodif.nii.gz
+
+cnt=0
+list="" 
+for i in $b;do
+    j=`echo $i | awk -F"E" 'BEGIN{OFMT="%10.10f"} {print $1 * (10 ^ $2)}' `
+    j=${j/.*}   
+    j=`echo "$j - 0" |bc | awk ' { if($1>=0) { print $1} else {print $1*-1 }}'`
+    if [ $j -lt 100 ];then
+	if [ "${list}" == "" ];then
+	    list="${cnt}"
+	else
+	    list="${list},${cnt}"
+	fi
+    fi
+    cnt=$(($cnt + 1))
+done
+
+echo $FSLDIR/bin/fslselectvols -i $d -o $o --vols=$list $5
+fslselectvols -i $d -o $o --vols=$list $5
 
 # Brain extraction before alignment
 bet nodif.nii.gz \
