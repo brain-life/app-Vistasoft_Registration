061e4541f918100809ccbb240159b2980f76a82c
Brad Caron <bacaron@imail.iu.edu>
2018-01-24 14:02:55 -0500

    dockerization edits

--- build.m
+++ build.m
@@ -1,4 +1,4 @@
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
-mcc -m -R -nodisplay -d compiled dtiinit
+mcc -m -R -nodisplay -d compiled main
 exit
--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ module load matlab/2017a
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
-mcc -m -R -nodisplay -d compiled dtiinit
+mcc -m -R -nodisplay -d compiled main
 exit
 END
 matlab -nodisplay -nosplash -r build
