e399adf4997f9b22edb681562a1d3a1fe7dd98d6
Brad Caron <bacaron@imail.iu.edu>
2018-02-07 15:12:35 -0500

    recompiled

--- compile.sh
+++ compile.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 module load matlab/2017a
 
+mkdir -p compiled
+
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
-<<<<<<< HEAD
 mcc -m -R -nodisplay -d compiled main
-=======
-mcc -m -R -nodisplay -d compiled vistasoftregistration
->>>>>>> 85040d38e475b65dfbbdeae5f056022f1ded5574
 exit
 END
 matlab -nodisplay -nosplash -r build
